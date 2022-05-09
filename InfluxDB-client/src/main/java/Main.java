c486cdbc129c75ecff24e005472a213e3b69f15d
KennethNorholm <70717116+KennethNorholm@users.noreply.github.com>
2022-05-09 15:02:41 +0200

    Update Main.java

--- InfluxDB-client/src/main/java/Main.java
+++ InfluxDB-client/src/main/java/Main.java
@@ -19,11 +19,9 @@ public class Main {
     private static final String bucket = "testbucket";
 
     private static final List<String> recordBatch = new ArrayList<>();
-
-    private static String delimiter;
     private static final Map<String, String> filePathNameMap = new HashMap<>();
-    private static WriteApi writeApi;
-
+    private static String delimiter;
+//    private static WriteApi writeApi;
 
     public static void main(String[] args) {
         ingestCSV(args);
@@ -33,7 +31,7 @@ public class Main {
     private static void ingestCSV(String[] args) {
         File sourceDirectory = new File(args[0]);
 
-        if (args.length > 1){
+        if (args.length > 1) {
             if (args[1] == null) {
                 System.out.println("Defaulting to sep=\" \"");
                 delimiter = " ";
@@ -45,38 +43,71 @@ public class Main {
             delimiter = " ";
         }
 
-        InfluxDBClient influxDBClient = InfluxDBClientFactory.create("http://localhost:8086", token, org, bucket);
-
-        writeApi = influxDBClient.makeWriteApi();
 
         List<File> csvFiles = getCsvFiles(sourceDirectory, "");
 
+        int maxNumberOfConcurrentThreads = 90;
+        int sleepTime = 2000;
+
+        List<Thread> workerThreads = new ArrayList<>();
+
         Stopwatch.setInitialStartTime();
-        writeCsvDataToInfluxDB(csvFiles);
+        for (File csvFile : csvFiles) {
+
+            if (workerThreads.size() < maxNumberOfConcurrentThreads) {
+
+                Thread thread = new Thread(() -> {
+                    InfluxDBClient influxDBClient = InfluxDBClientFactory.create("http://localhost:8086", token, org, bucket);
+                    WriteApi writeApi = influxDBClient.makeWriteApi();
+                    writeCsvDataToInfluxDB(csvFile, writeApi);
+                    influxDBClient.close();
+                });
+                workerThreads.add(thread);
+                thread.start();
+
+            } else {
+                while (maxNumberOfConcurrentThreads == workerThreads.size()) {
+
+                    ArrayList<Thread> deadThreads = new ArrayList<>();
+                    for (Thread workerThread : workerThreads) {
+                        if (!workerThread.isAlive())
+                            deadThreads.add(workerThread);
+                    }
+
+                    if (!deadThreads.isEmpty()) {
+                        workerThreads.removeAll(deadThreads);
+                    } else {
+                        try {
+                            Thread.sleep(sleepTime);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+
         Stopwatch.setEndTime();
 
         System.out.println("Time to ingest: " + Stopwatch.getTime());
-        influxDBClient.close();
     }
 
 
-    private static void writeCsvDataToInfluxDB(List<File> csvFiles) {
-        for (File file : csvFiles){
-            if (!file.exists()){
-                System.out.println("File: " + file.getAbsolutePath() + " does not exist");
-                continue;
-            }
+    private static void writeCsvDataToInfluxDB(File file, WriteApi writeApi) {
+        if (!file.exists()) {
+            System.out.println("File: " + file.getAbsolutePath() + " does not exist");
+            return;
+        }
 
-            try {
-                readFileToInflux(file);
-            } catch (Exception e){
-                System.out.println("Error reading file: " + file.getAbsolutePath() + e.getMessage());
-            }
+        try {
+            readFileToInflux(file, writeApi);
+        } catch (Exception e) {
+            System.out.println("Error reading file: " + file.getAbsolutePath() + e.getMessage());
         }
     }
 
 
-    private static void readFileToInflux(File file) throws Exception {
+    private static void readFileToInflux(File file, WriteApi writeApi) throws Exception {
         FileReader fileReader = new FileReader(file);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
@@ -84,18 +115,18 @@ public class Main {
 
         String fileLine;
         // Run through file
-        while ((fileLine = bufferedReader.readLine()) != null){
+        while ((fileLine = bufferedReader.readLine()) != null) {
             String[] splitLine = fileLine.split(delimiter);
-            if (splitLine.length == 2){
-                writeDataPointToInflux(measurement, splitLine[0].trim(), splitLine[1].trim());
+            if (splitLine.length == 2) {
+                writeDataPointToInflux(measurement, splitLine[0].trim(), splitLine[1].trim(), writeApi);
             } else {
-                writeDataPointToInflux(splitLine[0].trim(), splitLine[1].trim(), splitLine[2].trim());
+                writeDataPointToInflux(splitLine[0].trim(), splitLine[1].trim(), splitLine[2].trim(), writeApi);
             }
         }
     }
 
 
-    private static void writeDataPointToInflux(String measurement, String timestamp, String value) {
+    private static void writeDataPointToInflux(String measurement, String timestamp, String value, WriteApi writeApi) {
         String point = Point.measurement(measurement)
                 .addField("value", Double.parseDouble(value))
                 .time(Long.parseLong(timestamp), WritePrecision.MS).toLineProtocol();
@@ -110,19 +141,19 @@ public class Main {
     }
 
 
-    private static List<File> getCsvFiles(File sourceDirectory, String filePath){
+    private static List<File> getCsvFiles(File sourceDirectory, String filePath) {
         List<File> csvFiles = new ArrayList<>();
 
         File[] subFiles = sourceDirectory.listFiles();
 
-        if (subFiles == null){
+        if (subFiles == null) {
             if (sourceDirectory.isFile())
                 csvFiles.add(sourceDirectory);
             return csvFiles;
         }
 
-        for (File file : subFiles){
-            if (file.isDirectory()){
+        for (File file : subFiles) {
+            if (file.isDirectory()) {
                 csvFiles.addAll(getCsvFiles(file, filePath + "/" + file.getName()));
             } else if (file.isFile()) {
                 filePathNameMap.put(file.getAbsolutePath(), filePath + "/" + file.getName());
